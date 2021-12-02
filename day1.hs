13664014273ba54438703232b09fbfefd08dd954
David Fontenot <davidmfontenot@gmail.com>
2021-12-01 22:31:51 -0800

    super rusty Haskell coding

---
+++ day1.hs
@@ -0,0 +1,10 @@
+get2nd (_, a) = a
+
+acc :: (Maybe Int, Int) -> Int -> (Maybe Int, Int)
+acc (Nothing, _) curDepth = (Just curDepth, 0)
+acc (Just depth, count) curDepth = (Just curDepth, if curDepth > depth then count + 1 else count)
+
+main :: IO ()
+main = do
+  textData <- readFile "day1.txt"
+  putStrLn $ show $ get2nd $ foldl acc (Nothing, 0) $ map (read :: String -> Int) $ filter (/= "") $ lines textData
