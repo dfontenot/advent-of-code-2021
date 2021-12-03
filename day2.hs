81101000214bfeb885182a8af17d75cccca0e0cc
David Fontenot <davidmfontenot@gmail.com>
2021-12-03 00:31:08 -0800

    part one completed (with a warning)

--- day2.hs
+++ day2.hs
@@ -1,15 +1,13 @@
 #!/usr/bin/env stack
 {- stack script
  --resolver nightly-2019-12-22
- --package "split text"
+ --package "split"
  --ghc-options -Wall
 -}
-{-# LANGUAGE OverloadedStrings #-}
 
 module Day2 where
 
 import Data.List.Split
-import Data.Text hiding (splitOn, lines)
 
 data Direction = Forward | Down | Up deriving (Show)
 data Command = Command { direction :: Direction, count_ :: Int }
@@ -27,8 +25,18 @@ instance Read Command where
 instance Show Command where
   show (Command {direction=d, count_=cnt}) = (show d) ++ " " ++ (show cnt)
 
+processCommands :: [Command] -> (Int, Int)
+processCommands commands = processCommands' (0, 0) commands
+  where
+    processCommands' pos [] = pos
+    processCommands' (x, y) ((Command {direction=Forward, count_=cnt}):rst) = processCommands' (x + cnt, y) rst
+    processCommands' (x, y) ((Command {direction=Down, count_=cnt}):rst) = processCommands' (x, y + cnt) rst
+    processCommands' (x, y) ((Command {direction=Up, count_=cnt}):rst) = processCommands' (x, y - cnt) rst
+
+answer :: (Int, Int) -> Int
+answer (x, y) = x * y
+
 main :: IO ()
 main = do
   textData <- readFile "day2.txt"
-  putStrLn $ show $ (read ((lines textData) !! 0) :: Command)
-  --putStrLn $ show $ (lines textData) !! 0
+  putStrLn $ show $ answer $ processCommands $ map (read :: String -> Command) $ filter (/= "") $ lines textData
