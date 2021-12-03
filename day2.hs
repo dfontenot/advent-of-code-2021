b2ab747983dcf1417684fb395db782ef6cbbf030
David Fontenot <davidmfontenot@gmail.com>
2021-12-03 00:42:06 -0800

    completed part 2

--- day2.hs
+++ day2.hs
@@ -33,6 +33,14 @@ processCommands commands = processCommands' (0, 0) commands
     processCommands' (x, y) ((Command {direction=Down, count_=cnt}):rst) = processCommands' (x, y + cnt) rst
     processCommands' (x, y) ((Command {direction=Up, count_=cnt}):rst) = processCommands' (x, y - cnt) rst
 
+processCommands2 :: [Command] -> (Int, Int)
+processCommands2 commands = processCommands' (0, 0, 0) commands
+  where
+    processCommands' (x, y, _) [] = (x, y)
+    processCommands' (x, y, aim) ((Command {direction=Forward, count_=cnt}):rst) = processCommands' (x + cnt, y + cnt * aim, aim) rst
+    processCommands' (x, y, aim) ((Command {direction=Down, count_=cnt}):rst) = processCommands' (x, y, aim + cnt) rst
+    processCommands' (x, y, aim) ((Command {direction=Up, count_=cnt}):rst) = processCommands' (x, y, aim - cnt) rst
+
 answer :: (Int, Int) -> Int
 answer (x, y) = x * y
 
@@ -40,3 +48,4 @@ main :: IO ()
 main = do
   textData <- readFile "day2.txt"
   putStrLn $ show $ answer $ processCommands $ map (read :: String -> Command) $ filter (/= "") $ lines textData
+  putStrLn $ show $ answer $ processCommands2 $ map (read :: String -> Command) $ filter (/= "") $ lines textData
