473a3bf8c94f6270c9575bde1e892b659ef4742e
David Fontenot <davidmfontenot@gmail.com>
2021-12-03 23:20:18 -0800

    calculate the actual number

--- day3.hs
+++ day3.hs
@@ -2,10 +2,17 @@ module Day3 where
 
 data Binary = One | Zero deriving (Show)
 
+reverseList [] = []
+reverseList (x:xs) = reverseList xs ++ [x]
+
 bitToBinary :: Char -> Binary
 bitToBinary '0' = Zero
 bitToBinary '1' = One
 bitToBinary _ = error "invalid data"
+
+binaryToInt :: Binary -> Int
+binaryToInt Zero = 0
+binaryToInt One = 1
 
 mostCommon :: [Binary] -> Binary
 mostCommon nums = mostCommon' (0, 0) nums
@@ -15,11 +22,17 @@ mostCommon nums = mostCommon' (0, 0) nums
     mostCommon' (zeroes, ones) (Zero:xs) = mostCommon' (zeroes + 1, ones) xs
 
 -- TODO: cleanup
-findGamma :: [String] -> [Binary]
-findGamma ((char:[]):xs) = (mostCommon (map (bitToBinary . (!! 0) . (take 1)) ((char:[]):xs))) : []
-findGamma xs = (mostCommon (map (bitToBinary . (!! 0) . (take 1)) xs)) : (findGamma (map (drop 1) xs))
+getGammaBits :: [String] -> [Binary]
+getGammaBits ((char:[]):xs) = (mostCommon (map (bitToBinary . (!! 0) . (take 1)) ((char:[]):xs))) : []
+getGammaBits xs = (mostCommon (map (bitToBinary . (!! 0) . (take 1)) xs)) : (getGammaBits (map (drop 1) xs))
+
+readBinaryBits :: [Binary] -> Int
+readBinaryBits lst = readBinaryBits' (0, 0) $ reverseList lst
+  where
+    readBinaryBits' (sum, _) [] = sum
+    readBinaryBits' (sum, exp) (x:xs) = readBinaryBits' (sum + (binaryToInt x) * 2 ^ exp, exp + 1) xs
 
 main :: IO ()
 main = do
   textData <- readFile "day3.txt"
-  putStrLn $ show $ findGamma $ filter (/= "") $ lines textData
+  putStrLn $ show $ readBinaryBits $ getGammaBits $ filter (/= "") $ lines textData
