8bdf5613ebbac0de7cb9c07037e13f1acbd31ffe
David Fontenot <davidmfontenot@gmail.com>
2021-12-17 22:52:38 -0800

    cleanup

--- day4.hs
+++ day4.hs
@@ -3,7 +3,6 @@ module Main where
 
 import qualified Data.Vector as V
 
-import Debug.Trace
 import Data.List
 import Text.Parsec.String (Parser)
 import Text.ParserCombinators.Parsec hiding (many)
@@ -25,20 +24,8 @@ instance Show Matrix where
       listLines = V.toList $ V.map (\line -> intercalate " " (listLine line)) lines_
       listLine line = if (null line) then ["(empty line)"] else V.toList $ V.map show line
 
-whitespace :: Parser ()
-whitespace = void $ oneOf " \n\t"
-
-lexeme :: Parser a -> Parser a
-lexeme p = p <* whitespace
-
-symbol :: Char -> Parser Char
-symbol c = lexeme $ char c
-
 integer :: Parser Integer
---integer = read <$> lexeme (many1 digit)
 integer = read <$> many1 digit
-
-commaSep p  = p `sepBy` (symbol ',')
 
 bingoLine :: Parser (V.Vector Integer)
 bingoLine = do
@@ -46,8 +33,8 @@ bingoLine = do
   nums <- integer `sepBy` (many1 (char ' '))
   return $ V.fromList nums
 
-bingoCard' :: Parser Matrix
-bingoCard' = do
+bingoCard :: Parser Matrix
+bingoCard = do
   line1 <- bingoLine
   void $ char '\n'
   line2 <- bingoLine
@@ -59,49 +46,22 @@ bingoCard' = do
   line5 <- bingoLine
   return $ Matrix $ V.fromList [line1, line2, line3, line4, line5]
 
-bingoCard :: Parser [Matrix]
-bingoCard = do
-  lines_ <- (bingoLine `sepBy1` (char '\n')) `endBy` string "\n\n"
-  --lines_ <- (bingoLine `endBy` string "\n\n") `sepBy1` char '\n'
-  --lines_ :: () -- lines_ is [[V.Vector Integer]]
-  -- -- TODO: there has to be a better way...
-  -- line1 <- bingoLine `sepBy` char '\n'
-  -- line2 <- bingoLine `sepBy` char '\n'
-  -- line3 <- bingoLine `sepBy` char '\n'
-  -- line4 <- bingoLine `sepBy` char '\n'
-  -- line5 <- bingoLine `sepBy` char '\n'
-  --_ <- many1 $ char '\n'
-  --lines_ <- manyTill bingoLine (try (string "\n\n"))
-  --lines_ <- bingoLine `sepBy` (char '\n')
-  --whitespace
-  --return $ Matrix (V.fromList lines_)
-  --return $ Matrix [V.fromList [V.fromList [1,2]]]
-  return $ map (\card -> Matrix $ V.fromList card) lines_
-    -- where
-    --   matrixify matrix = Matrix $ map V.fromList matrix
-
 bingoCards' :: Parser [Matrix]
 bingoCards' = do
-  mat <- bingoCard'
+  mat <- bingoCard
   rstMat <- rstBingoCards
   return (mat:rstMat)
     where
-      rstBingoCards = try ((many1 (char '\n')) >> bingoCards') <|> (return (trace "base case" []))
+      rstBingoCards = try ((many1 (char '\n')) >> bingoCards') <|> (return [])
 
 bingoFile :: Parser Parsed
 bingoFile = do
-  --header <- commaSep integer
   header <- integer `sepBy` (char ',')
   _ <- many1 $ char '\n'
-  --bingoCards_ <- bingoCard' `sepBy` (string "\n\n")
-  --bingoCards_ <- bingoCard' `sepBy` (string "\n\n")
-  bingoCards_ <- (trace "bingo" bingoCards')
+  bingoCards_ <- bingoCards'
   skipMany $ char '\n'
-  --_ <- many $ char '\n'
   eof
-  --bingoCards_ <- bingoCard
   return $ Parsed {bingoNumbers=header, bingoCards=bingoCards_}
-  --skipMany1 $ char '\n'
 
 parseInput :: String -> Either ParseError Parsed
 parseInput input = parse bingoFile "day4.txt" input -- 2nd arg is just the filename to use in parseerror s
