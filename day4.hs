14135a0af8c6fc5a8598eadb1a81b99d804499ec
David Fontenot <davidmfontenot@gmail.com>
2021-12-18 11:32:09 -0800

    parsed state into a playable gamestate

--- day4.hs
+++ day4.hs
@@ -12,13 +12,24 @@ import Text.Parsec.Char (digit, oneOf, char)
 import Control.Applicative (many)
 import Control.Monad (void)
 
-newtype Matrix = Matrix (V.Vector (V.Vector Integer))
-data Parsed = Parsed { bingoNumbers :: [Integer], bingoCards :: [Matrix] }
+-- types to parse into
+newtype Matrix a = Matrix (V.Vector (V.Vector a))
+data Parsed = Parsed { bingoNumbers :: [Integer], bingoCards :: [Matrix Integer] }
+
+-- gameplay types
+data Cell = Cell { cellNum :: Integer, cellMarked :: Bool }
+data Gamestate = Gamestate { bingoNums :: [Integer], bingoCardz :: [Matrix Cell] }
+
+instance Show Gamestate where
+  show (Gamestate {bingoNums=nums, bingoCardz=cards}) = show ("nums: " ++ (show nums)) ++ "\n" ++ (intercalate "\n" (map show cards))
+
+instance Show Cell where
+  show (Cell {cellNum=num, cellMarked=marked}) = "[" ++ (if marked then "(" ++ (show num) ++ ")" else (show num)) ++ "]"
 
 instance Show Parsed where
   show (Parsed {bingoNumbers=calledNumbers, bingoCards=cards}) = show calledNumbers ++ "\n" ++ (intercalate "\n\n" (map show cards))
 
-instance Show Matrix where
+instance (Show a) => Show (Matrix a) where
   show (Matrix lines_) = "----\n" ++ (intercalate "\n" listLines) ++ "\n----"
     where
       listLines = V.toList $ V.map (\line -> intercalate " " (listLine line)) lines_
@@ -33,7 +44,7 @@ bingoLine = do
   nums <- integer `sepBy` (many1 (char ' '))
   return $ V.fromList nums
 
-bingoCard :: Parser Matrix
+bingoCard :: Parser (Matrix Integer)
 bingoCard = do
   line1 <- bingoLine
   void $ char '\n'
@@ -46,7 +57,7 @@ bingoCard = do
   line5 <- bingoLine
   return $ Matrix $ V.fromList [line1, line2, line3, line4, line5]
 
-bingoCards' :: Parser [Matrix]
+bingoCards' :: Parser [Matrix Integer]
 bingoCards' = do
   mat <- bingoCard
   rstMat <- rstBingoCards
@@ -66,7 +77,15 @@ bingoFile = do
 parseInput :: String -> Either ParseError Parsed
 parseInput input = parse bingoFile "day4.txt" input -- 2nd arg is just the filename to use in parseerror s
 
+initialGamestate :: Parsed -> Gamestate
+initialGamestate (Parsed {bingoNumbers=calledNumbers, bingoCards=cards}) = Gamestate {bingoNums=calledNumbers, bingoCardz=map matToCells cards}
+  where
+    matToCells (Matrix mat) = Matrix $ V.map (\row -> V.map (\num -> Cell { cellNum=num, cellMarked=False }) row) mat
+
 main :: IO ()
 main = do
   textData <- readFile "day4.txt"
-  putStrLn $ show $ parseInput textData
+  let parsed = parseInput textData in
+      case parsed of
+        Right result -> putStrLn $ show $ initialGamestate result
+        Left err -> putStrLn $ show $ err
