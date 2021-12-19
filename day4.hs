dcee4227e981b1c72ca1e5d1b2af53a206f13960
David Fontenot <davidmfontenot@gmail.com>
2021-12-19 00:35:05 -0800

    working on bingo mechanics

--- day4.hs
+++ day4.hs
@@ -5,12 +5,13 @@ import qualified Data.Vector as V
 
 import Data.List
 import Text.Parsec.String (Parser)
-import Text.ParserCombinators.Parsec hiding (many)
+import Text.ParserCombinators.Parsec hiding (many, State)
 import Text.Parsec.Combinator (many1)
 import Text.Parsec.Char (digit, oneOf, char)
 
 import Control.Applicative (many)
 import Control.Monad (void)
+import Control.Monad.State
 
 -- types to parse into
 newtype Matrix a = Matrix (V.Vector (V.Vector a))
@@ -18,10 +19,7 @@ data Parsed = Parsed { bingoNumbers :: [Integer], bingoCards :: [Matrix Integer]
 
 -- gameplay types
 data Cell = Cell { cellNum :: Integer, cellMarked :: Bool }
-data Gamestate = Gamestate { bingoNums :: [Integer], bingoCardz :: [Matrix Cell] }
-
-instance Show Gamestate where
-  show (Gamestate {bingoNums=nums, bingoCardz=cards}) = show ("nums: " ++ (show nums)) ++ "\n" ++ (intercalate "\n" (map show cards))
+type Gamestate = [(Matrix Cell)]
 
 instance Show Cell where
   show (Cell {cellNum=num, cellMarked=marked}) = "[" ++ (if marked then "(" ++ (show num) ++ ")" else (show num)) ++ "]"
@@ -35,6 +33,7 @@ instance (Show a) => Show (Matrix a) where
       listLines = V.toList $ V.map (\line -> intercalate " " (listLine line)) lines_
       listLine line = if (null line) then ["(empty line)"] else V.toList $ V.map show line
 
+-- parsing stuff
 integer :: Parser Integer
 integer = read <$> many1 digit
 
@@ -77,10 +76,41 @@ bingoFile = do
 parseInput :: String -> Either ParseError Parsed
 parseInput input = parse bingoFile "day4.txt" input -- 2nd arg is just the filename to use in parseerror s
 
+-- running the bingo game stuff
 initialGamestate :: Parsed -> Gamestate
-initialGamestate (Parsed {bingoNumbers=calledNumbers, bingoCards=cards}) = Gamestate {bingoNums=calledNumbers, bingoCardz=map matToCells cards}
+initialGamestate (Parsed {bingoNumbers=_, bingoCards=cards}) = map matToCells cards
   where
     matToCells (Matrix mat) = Matrix $ V.map (\row -> V.map (\num -> Cell { cellNum=num, cellMarked=False }) row) mat
+
+rotateMat :: (Matrix a) -> (Matrix a)
+rotateMat (Matrix mat) = Matrix $ V.fromList $ collectMat 0 (V.length (mat V.! 0)) mat
+  where
+    collectMat i len mat | i <= len - 1 = (V.map (\row -> row V.! i) mat):(collectMat (i + 1) len mat)
+    collectMat _ _ _ = []
+
+checkRows :: (Matrix Cell) -> Bool
+checkRows (Matrix x) = V.any (V.map (\row -> V.all cellMarked) x)
+
+winningCard :: Gamestate -> Maybe (Matrix Cell)
+winningCard [] = Nothing
+winningCard (x:xs) = if checkRows x || (checkRows . rotateMat) x then Just x else winningCard xs
+
+unmarkedSum :: (Matrix Cell) -> Integer
+unmarkedSum (Matrix mat) = V.sum $ V.map (\row -> V.sum (V.map (\c -> if cellMarked c then 0 else cellNum c) row)) mat
+
+runBingo :: [Integer] -> State Gamestate (Maybe Integer)
+runBingo [] = return Nothing
+runBingo (x:xs) = do
+  cards <- get
+  -- put $ map (recordNum x) cards
+  cards' <- get
+  let scored = fmap ((*x) . unmarkedSum) (winningCard cards') in
+      case scored of
+        Nothing -> runBingo xs
+        _ -> return scored
+  -- case (winningCard cards') of
+  --   Just card -> return $ Just $ (unmarkedSum card) * x
+  --   Nothing -> runBingo xs
 
 main :: IO ()
 main = do
